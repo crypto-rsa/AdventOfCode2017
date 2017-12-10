30207569dba2e49e629d8690695ebfbd098c5aa3
crypto_rsa <pkotrc@gmail.com>
2017-12-10 08:53:57 +0100

    Add project files.

---
+++ Advent Of Code 2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2017
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
